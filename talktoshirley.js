11258441a7f534706ae26764d6aaee9ea70f8e45
IMS <iainsear22@hotmail.com>
2026-05-22 04:54:31 +0100

    Mobile update

--- talktoshirley.js
+++ talktoshirley.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function($) {
-	$(".scroll").click(function(event){
+	$('#menu-toggle').click(function(event) {
+		event.preventDefault();
+		var expanded = $(this).attr('aria-expanded') === 'true';
+		$(this).attr('aria-expanded', expanded ? 'false' : 'true');
+		$('#main-nav').toggleClass('open');
+	});
+
+	$('.scroll').click(function(event){
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top-60}, 500);
+		if ($('#main-nav').hasClass('open')) {
+			$('#main-nav').removeClass('open');
+			$('#menu-toggle').attr('aria-expanded', 'false');
+		}
 	});
 });
